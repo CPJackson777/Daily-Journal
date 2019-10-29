874fbb7cfb9bc9cfdaae7c356f161ee1595a8dce
Charles Jackson <cpjackson572@gmail.com>
2019-10-29 15:51:13 -0500

    successfully added event listener to Record Journal Entry button

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -11,3 +11,9 @@ import renderDom from "./entriesDOM.js"
 
 API.getJournalEntries()
 .then(response => renderDom.renderJournalEntries(response))
+
+document.getElementById("record-entry").addEventListener("click", function(event) {
+    event.preventDefault()
+    console.log("You clicked on the Record Journal Entry button")
+})
+// Regarding line 16...any time you are adding an event listener to a button that is part of a form within index.html, you must add event.preventDefault() to the event listener because otherwise, the page will refresh and delete your console.log or whatever else you have in the event listener.
