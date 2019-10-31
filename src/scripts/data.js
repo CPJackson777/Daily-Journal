a420e2d904a50d6ff13311a5afd447cb5befe58a
Charles Jackson <cpjackson572@gmail.com>
2019-10-31 12:11:05 -0500

    Got POST fetch calls set up properly in data.js. Entry still not showing on api though .

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,9 +1,19 @@
 const API = {
-  getJournalEntries () {
-      return fetch("http://localhost:3000/entries")
-          .then(response => response.json())
+  getJournalEntries: function () {
+    return fetch("http://localhost:3000/entries")
+      .then(response => response.json())
+
+  },
+  saveNewJournalEntry: function (newJournalEntry) {
+    return fetch("http://localhost:3000/entries", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newJournalEntry)
+    })
   }
 }
 
-export default API
 
+export default API
