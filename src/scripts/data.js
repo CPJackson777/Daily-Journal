48be0008006b8994f474aa583e81bec9fe338b48
Charles Jackson <cpjackson572@gmail.com>
2019-11-04 22:29:17 -0600

    added a couple comments

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,9 +1,13 @@
+//      Set up API object. Fetch all entries
+
 const API = {
   getJournalEntries: function () {
     return fetch("http://localhost:3000/entries")
       .then(response => response.json())
-
   },
+
+//      Post a new entry when the form is filled out
+
   saveJournalEntry: function (newJournalEntry) {
     return fetch("http://localhost:3000/entries", {
       method: "POST",
@@ -15,5 +19,6 @@ const API = {
   }
 }
 
-
+//      Exports API object so I can access it in another js by importing it
 export default API
+
